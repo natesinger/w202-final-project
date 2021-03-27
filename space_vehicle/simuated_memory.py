2e37ae9fb1193c5faf43c93b4d9a6a72ab9c9422
Nate Singer <nathaniel@singer.cloud>
2021-03-26 21:10:26 -0500

    memory parser beta v0.1b

--- space_vehicle/simuated_memory.py
+++ space_vehicle/simuated_memory.py
@@ -1,38 +1,84 @@
+#!/usr/bin/python3
 
-class SimulatedMemory:
+class MemoryManager(object):
+    """"""
     def __init__(self):
-        with open('.tmp/tmp_nvram.bin','a+') as io:
-            pass
+        self.simulated_executablebinary = None
+        self.running_memory = None
+        self.selectedkey = None
+        self.io_stream = None
+        self.memory_filename = "space_vehicle/nvram.bin" #from project root
 
-        #this is going to be a config file as a raw binary file so we can demonstrate exactly how small this memory requirement is
+    def __enter__(self):
+        self.io_stream = open(self.memory_filename,"r+b")
+        return self
 
-    #    self.keyspace = []
-    #    for i in range(10): #setup keyspace
-            #gen a key
-    #        pass
+    def __exit__(self, x,y,z): #junk variables to carry null exit params
+        self.io_stream.close()
+        return self
 
-    def read_key():
-        pass
+    def cycle_stream_mode(self, read:bool=False):
+        """because of bugs in python3's binary read/write this is necessary"""
+        if read:
+            self.io_stream.close()
+            self.io_stream = open(self.memory_filename,"r+b")
+        else:
+            self.io_stream.close()
+            self.io_stream = open(self.memory_filename,"w+b")
 
-    def write_key():
-        pass
+    def write_patch(self, patch_str:str, offset:int):
+        """in python we cannot overwite specific spot in file, so we must read
+        the entire file into memory patch in memory, and then write back out"""
+        self.cycle_stream_mode(True) #set io as read
+        old_file = self.io_stream.read()
 
+        old_file_front = old_file[:offset]
+        old_file_back = old_file[offset+len(patch_str):]
+
+        new_file = old_file_front + patch_str + old_file_back
+        assert len(new_file) == len(old_file)
+
+        self.cycle_stream_mode(False) #set io as write
+        self.io_stream.write(new_file)
+
+    def read_keyselection(self):
+        self.cycle_stream_mode(True) #set io to read
+        return self.io_stream.read()[15*32:(15*32)+1]
+
+    def write_keyselection(self, selection:int):
+        selection = int(selection).to_bytes(1, byteorder='big')
+        index = 15*32
+        self.write_patch(selection, index)
+
+    def read_key(self):
+        index = self.read_keyselection()
+        position = 32*int.from_bytes(index,byteorder='big')
+
+        self.cycle_stream_mode(True) #set io to read
+        return self.io_stream.read()[position:position+32]
+
+    def write_key(self, key_str:str):
+        index = self.read_keyselection()
+        position = 32*int.from_bytes(index,byteorder='big')
+
+        self.cycle_stream_mode(True) #set io to read
+        self.write_patch(key_str, position)
+
+    def dump(self):
+        self.cycle_stream_mode(True) #set io to read
+        return self.io_stream.read()
+
+    def clear(self):
+        self.io_stream.seek(0,0) #set io head at row 0 col 0
+        self.io_stream.truncate() #clear the file
+        self.io_stream.write(b'\xFF'*512)
+
+def run():
+    with MemoryManager() as m:
+        #print(m.read_keyselection())
+        m.write_key(b'\x42'*32)
+        print(m.dump())
+        #m.clear()
 
 if __name__ == "__main__":
-    emulated_ram = SimulatedMemory()
-
-
-#we can do this as a class maybe?
-
-#I want to use a YAML config file to store the keys, and settings simulating NVRAM and operating memory
-
-
-
-
-#256/32 = 8
-#so for 10 keys we need 32*10 byte
-
-
-#MUST HAVE:
-    #KEY SPACE for up to 15 keys
-    #SELECTED KEY
+    run()
